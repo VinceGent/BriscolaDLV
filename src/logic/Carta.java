4cb7c786ef5a22c9964ac696874d123f152e29ab
Vincenzo <Vincenzo@pc-vincenzo>
2015-12-11 17:46:04 +0100

    aggiunte immagini e classe mazzo e carta

--- src/logic/Carta.java
+++ src/logic/Carta.java
@@ -1,12 +1,51 @@
 package logic;
 
+import javafx.scene.image.Image;
+
 public class Carta {
 	
+	private int id;
+	private String seme;
+	private int valore;
+	private Image img;
 	
-	
-	
-	public Carta(){
-		
+	public Carta(int id, String seme, int valore, Image img){
+		this.setId(id);
+		this.setSeme(seme);
+		this.setValore(valore);
+		this.setImg(img);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSeme() {
+		return seme;
+	}
+
+	public void setSeme(String seme) {
+		this.seme = seme;
+	}
+
+	public int getValore() {
+		return valore;
+	}
+
+	public void setValore(int valore) {
+		this.valore = valore;
+	}
+
+	public Image getImg() {
+		return img;
+	}
+
+	public void setImg(Image img) {
+		this.img = img;
 	}
 	
 	
---
+++ src/logic/Mazzo.java
@@ -0,0 +1,82 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import javafx.scene.image.Image;
+
+public class Mazzo {
+
+	private List<Carta> mazzo;
+	private List<String> semi;
+	private List<Integer> valori;
+	
+	public Mazzo(){
+		this.setMazzo(new ArrayList<Carta>());
+		this.semi = new ArrayList<String>();
+		this.valori = new ArrayList<Integer>();
+		inizializzaSemi();
+		inizializzaValori();
+		creaCarte();
+
+	}
+	
+	private void creaCarte(){
+		
+		for (int i = 0; i < semi.size(); i++) {
+			for (int j = 0; j < valori.size(); j++) {
+				String immagine = "file:/img/"+ Integer.toString(j+1)+ semi.get(i)+".png";
+//				Image img = new Image(immagine);
+				getMazzo().add(new Carta(j+1, semi.get(i), valori.get(j), null));
+			}
+		}
+		
+
+	}
+	
+	private void inizializzaValori(){
+		valori.add(11);
+		valori.add(0);
+		valori.add(10);
+		valori.add(0);
+		valori.add(0);
+		valori.add(0);
+		valori.add(0);
+		valori.add(2);
+		valori.add(3);
+		valori.add(4);
+	}
+	private void inizializzaSemi(){
+		semi.add("denari");
+		semi.add("bastoni");
+		semi.add("coppe");
+		semi.add("spade");
+	}
+
+	public void mischiaMazzo(){
+		long seed = System.nanoTime();
+        Collections.shuffle(mazzo, new Random(seed));
+	}
+	
+	public static void main(String[] args) {
+	/*	
+		Mazzo mazzo = new Mazzo();
+//		mazzo.mischiaMazzo();
+		for (Carta c : mazzo.getMazzo()) {
+			System.out.println(c.getId() + "  " + c.getSeme() + "  "+ c.getValore());
+		}
+		*/
+	}
+
+	public List<Carta> getMazzo() {
+		return mazzo;
+	}
+
+	public void setMazzo(List<Carta> mazzo) {
+		this.mazzo = mazzo;
+	}
+	
+	
+}
